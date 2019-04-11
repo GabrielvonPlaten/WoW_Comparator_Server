08296cb35b6a0c9ef3a8c6ee485360d0814025ab
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-04-11 05:22:18 +0200

    Hash admin password and add login route

--- routes/Admin.js
+++ routes/Admin.js
@@ -2,17 +2,32 @@ const express = require('express');
 const Admin = require('../models/Admin');
 const router = new express.Router();
 
-// Create User
+// Create Admin User
 router.post('/register/admin', async (req, res) => {
-  const user = new Admin(req.body);
+  const admin = new Admin(req.body);
 
   try {
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
+    await admin.save();
+    const token = await admin.generateAuthToken();
+    res.status(201).send({ admin, token });
   } catch (err) {
     res.status(400).send(err);
   };
+});
+
+router.post('/admin/login', async (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+
+  try {
+    console.log(name, password)
+    const user = await Admin.findByCredentials(name, password);
+    const token = await user.generateAuthToken();
+
+    res.send({ user, token });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
