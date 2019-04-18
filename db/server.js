aa11407dde9017ca694086df5c40d66039ccf1db
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-04-18 03:29:36 +0200

    Add mlab server

--- db/server.js
+++ db/server.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wow_comparator', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-
-// mongoose.connect('mongodb://gabriel:nuxt1234@ds119606.mlab.com:19606/nuxt-testing-app', {
+// mongoose.connect('mongodb://127.0.0.1:27017/wow_comparator', {
 //   useNewUrlParser: true,
 //   useCreateIndex: true,
 //   useFindAndModify: false,
 // });
+
+
+mongoose.connect('mongodb://heroku_v2pfv6l0:ljj3neqr6pqaie3nvslqsituqq@ds115569.mlab.com:15569/heroku_v2pfv6l0', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 module.exports = {
   mongoose,
 }
