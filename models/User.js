b92ee3d44b1263a6196210e6f5bd60857dd25863
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-05-11 16:43:22 +0200

    Create User model

---
+++ models/User.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+    minlength: 1,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 10,
+    trim: true,
+    validate(val) {
+      if (val.toLowerCase().includes('password') || val.toLowerCase().includes('admin') || val.toLowerCase().includes('root')) {
+        throw new Error('Password cannot be "Password", "admin" or "root"');
+      }
+    },
+  },
+
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    validate(val) {
+      if (!validator.isEmail(val)) {
+        throw new Error('Email is invalid')
+      }
+    },
+  },
+
+  favoriteChars: [{
+    character: {
+      name: {
+        type: String,
+      },
+      realm: {
+        type: String,
+      },
+    },
+  }],
+
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    }
+  }],
+}, {
+  timestamps: true
+});
+
+// Find Admin by credentials
+userSchema.statics.findByCredentials = async (email, password) => {
+  const admin = await Admin.findOne({ email });
+
+  if (!admin) {
+    throw new Error('Unable to log in');
+  }
+
+  const isMatch = await bcrypt.compare(password, admin.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to log in');
+  };
+
+  return user;
+}
+
+// Generate Auth Session Token
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "2r27rQl86shnp7q", { expiresIn: '1 days'})
+
+  user.tokens = [...user.tokens, {token}];
+  await user.save();
+
+  return token;
+};
+
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  };
+
+  next();
+})
+
+let User = mongoose.model('User', userSchema);
+module.exports = User;
