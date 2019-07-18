30b84b0aee4848214bce6ab11e8a6bc6ac3950b8
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-18 19:35:12 +0200

    Update website to WoW's 8.2 patch

--- db/server.js
+++ db/server.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-DB_URI = process.env.NODE_ENV === 'production' ? process.env.DB_URI_PROD : process.env.DB_URI_DEV 
+DB_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.DB_URI_PROD
+    : process.env.DB_URI_DEV;
 
-mongoose.connect(DB_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb://heroku_v2pfv6l0:ljj3neqr6pqaie3nvslqsituqq@ds115569.mlab.com:15569/heroku_v2pfv6l0",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 module.exports = {
-  mongoose,
-}
+  mongoose
+};
